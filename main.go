315592bd20f7be7ae29df6258a7295d70f94885f
Bruno Bigras <bigras.bruno@gmail.com>
2021-06-19 21:50:48 -0400

    bleh

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"context"
 	"crypto/rand"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
-	"flag"
-	// "time"
+	"time"
 
 	ma "github.com/multiformats/go-multiaddr"
 	// crypto "github.com/libp2p/go-libp2p-crypto"
@@ -16,54 +16,42 @@ import (
 	"github.com/textileio/go-threads/api/client"
 	"github.com/textileio/go-threads/core/thread"
 
-	// db2 "github.com/textileio/go-threads/db"
-	// "github.com/alecthomas/jsonschema"
+	db2 "github.com/textileio/go-threads/db"
+	// "github.com/textileio/go-threads/db/options"
+	"github.com/alecthomas/jsonschema"
 	"google.golang.org/grpc"
 )
 
 type Person struct {
-    ID        string `json:"_id"`
-    Name      string `json:"name"`
-    Age       int    `json:"age"`
-    CreatedAt int    `json:"created_at"`
+	ID        string `json:"_id"`
+	Name      string `json:"name"`
+	Age       int    `json:"age"`
+	CreatedAt int    `json:"created_at"`
+}
+
+func getThread(name string, db0 *client.Client, dbs map[thread.ID]db2.Info) *thread.ID {
+	for threadID := range dbs {
+
+		dbInfo, errInfo := db0.GetDBInfo(context.Background(), threadID)
+		if errInfo != nil {
+			panic(errInfo)
+		}
+
+		if dbInfo.Name == name {
+			return &threadID
+		}
+	}
+	return nil
 }
 
 func main() {
 	db, err := client.NewClient("127.0.0.1:6006", grpc.WithInsecure())
 	if err != nil {
-        panic(err)
+		panic(err)
 	}
 
-	var nFlag = flag.Bool("info", false, "show current DB info")
+	var nFlag = flag.Int("c", 1, "computer number (1 or 2)")
 	flag.Parse()
-
-	if *nFlag {
-		dbs, errDbs := db.ListDBs(context.Background())
-		if errDbs != nil {
-			panic(errDbs)
-		}
-
-		for threadID := range dbs {
-			fmt.Println("thread", threadID)
-
-			dbInfo, errInfo := db.GetDBInfo(context.Background(), threadID)
-			if errInfo != nil {
-				panic(errInfo)
-			}
-			fmt.Println("addrs", dbInfo.Addrs)
-			fmt.Println("key", dbInfo.Key)
-
-			collections, errCol := db.ListCollections(context.Background(), threadID)
-			if errCol != nil {
-				panic(errCol)
-			}
-			for one := range collections {
-				fmt.Println("collection> ", one)
-			}
-
-		}
-		panic("done")
-	}
 
 	_, err7 := os.Stat("privkey")
 	if err7 != nil {
@@ -101,126 +89,193 @@ func main() {
 	// threadToken, err3 := db.GetToken(context.Background(), myIdentity)
 	_, err3 := db.GetToken(context.Background(), myIdentity)
 	if err3 != nil {
-        panic(err3)
-		}
-	/*
-
-	thread_pub_key, err10 := threadToken.PubKey()
-	if err10 != nil {
-		panic(err10)
-	}
-	fmt.Println("thread token", thread_pub_key)
-
-	_, err12 := os.Stat("threadid")
-	if err12 != nil {
-		fmt.Println("new thread")
-		threadID := thread.NewIDV1(thread.Raw, 32)
-
-		err11 := ioutil.WriteFile("threadid", threadID.Bytes(), 0600)
-		if err11 != nil {
-			panic(err11)
-		}
-
-		err2 := db.NewDB(context.Background(), threadID)
-		if err2 != nil {
-			panic(err2)
-		}
+		panic(err3)
 	}
 
-	data2, err13 := ioutil.ReadFile("threadid")
-	if err13 != nil {
-		panic(err13)
-	}
+	// computer 1
+	if *nFlag == 1 {
+		dbs, errDbs := db.ListDBs(context.Background())
+		if errDbs != nil {
+			panic(errDbs)
+		}
 
-	// threadID := thread.ID(string(data2))
-	threadID := thread.ID(data2)
-	*/
+		threadID := getThread("my-database", db, dbs)
+		fmt.Println("threadID", threadID)
 
+		if threadID == nil {
+			fmt.Println("create database")
+			threadID2 := thread.NewIDV1(thread.Raw, 32)
+			options := db2.WithNewManagedName("my-database")
+			err2 := db.NewDB(context.Background(), threadID2, options)
+			if err2 != nil {
+				panic(err2)
+			}
+			threadID = &threadID2
+		}
 
-	// on another computer
-	// dbInfo, err := db.GetDBInfo(context.Background(), threadID)
-	// fmt.Println("addrs", dbInfo.Addrs)
-	// fmt.Println("key", dbInfo.Key)
-	// err14 := db.NewDBFromAddr(context.Background(), dbInfo.Addrs[0], dbInfo.Key)
-	// if err14 != nil {
+		collection_exists := false
+		collections, errCol := db.ListCollections(context.Background(), *threadID)
+		if errCol != nil {
+			panic(errCol)
+		}
+		for one := range collections {
+			fmt.Println("collection> ", collections[one].Name)
+			if collections[one].Name == "Persons" {
+				collection_exists = true
+			}
+		}
+
+		if !collection_exists {
+			fmt.Println("create collection")
+			reflector := jsonschema.Reflector{}
+			mySchema := reflector.Reflect(&Person{}) // Generate a JSON Schema from a struct
+
+			err15 := db.NewCollection(context.Background(), *threadID, db2.CollectionConfig{
+				Name:   "Persons",
+				Schema: mySchema,
+				Indexes: []db2.Index{{
+					Path:   "name", // Value matches json tags
+					Unique: true,   // Create a unique index on "name"
+				}},
+			})
+			if err15 != nil {
+				panic(err15)
+			}
+		}
+
+		/*
+
+			thread_pub_key, err10 := threadToken.PubKey()
+			if err10 != nil {
+				panic(err10)
+			}
+			fmt.Println("thread token", thread_pub_key)
+
+			_, err12 := os.Stat("threadid")
+			if err12 != nil {
+				fmt.Println("new thread")
+				threadID := thread.NewIDV1(thread.Raw, 32)
+
+				err11 := ioutil.WriteFile("threadid", threadID.Bytes(), 0600)
+				if err11 != nil {
+					panic(err11)
+				}
+
+				err2 := db.NewDB(context.Background(), threadID)
+				if err2 != nil {
+					panic(err2)
+				}
+			}
+
+			data2, err13 := ioutil.ReadFile("threadid")
+			if err13 != nil {
+				panic(err13)
+			}
+
+			// threadID := thread.ID(string(data2))
+			threadID := thread.ID(data2)
+		*/
+
+		// on another computer
+		// dbInfo, err := db.GetDBInfo(context.Background(), threadID)
+		// fmt.Println("addrs", dbInfo.Addrs)
+		// fmt.Println("key", dbInfo.Key)
+		// err14 := db.NewDBFromAddr(context.Background(), dbInfo.Addrs[0], dbInfo.Key)
+		// if err14 != nil {
 		// panic(err14)
-	// }
+		// }
 
-	// fmt.Println("threadID", threadID)
+		// fmt.Println("threadID", threadID)
 
-	// addr, err22 := ma.NewMultiaddr("/ip4/172.27.0.3/tcp/4006/p2p/12D3KooWQZUHsPQzcyzydWLcabNwKb9zhvgsYSfBwDTB3cZi33kT/thread/bafktrg35keacmar4neyegxcinck77z3scjctjmipkpkovyzi6g52coq")
+		// addr, err22 := ma.NewMultiaddr("/ip4/172.27.0.3/tcp/4006/p2p/12D3KooWQZUHsPQzcyzydWLcabNwKb9zhvgsYSfBwDTB3cZi33kT/thread/bafktrg35keacmar4neyegxcinck77z3scjctjmipkpkovyzi6g52coq")
 
-	// addr, err22 := ma.NewMultiaddr("/ip4/100.67.149.7/tcp/4006/p2p/12D3KooWQZUHsPQzcyzydWLcabNwKb9zhvgsYSfBwDTB3cZi33kT/thread/bafktrg35keacmar4neyegxcinck77z3scjctjmipkpkovyzi6g52coq")
-	addr, err22 := ma.NewMultiaddr("/ip4/100.67.149.7/tcp/4006/p2p/12D3KooWQZUHsPQzcyzydWLcabNwKb9zhvgsYSfBwDTB3cZi33kT/thread/bafktrg35keacmar4neyegxcinck77z3scjctjmipkpkovyzi6g52coq")
-	if err22 != nil {
-		panic(err22)
+		// addr, err22 := ma.NewMultiaddr("/ip4/100.67.149.7/tcp/4006/p2p/12D3KooWQZUHsPQzcyzydWLcabNwKb9zhvgsYSfBwDTB3cZi33kT/thread/bafktrg35keacmar4neyegxcinck77z3scjctjmipkpkovyzi6g52coq")
+
+		// computer 2
+		/*
+
+		 */
+
+		/*
+			err := db.UpdateCollection(context.Background(), myThreadID, db.CollectionConfig{
+			    Name:    "Persons",
+			    Schema:  mySchema,
+			    Indexes: []db.Index{{
+			        Path:   "name",
+			        Unique: true,
+			    },
+			    {
+			        Path: "created_at", // Add an additional index on "created_at"
+			    }},
+				})
+		*/
+
+		query := db2.Where("name").Eq("Alice")
+		results, err16 := db.Find(context.Background(), *threadID, "Persons", query, &Person{})
+		if err16 != nil {
+			panic(err16)
+		}
+
+		alice := results.([]*Person)
+
+		if len(alice) == 0 {
+			fmt.Println("add Alice")
+			alice := &Person{
+				ID:        "",
+				Name:      "Alice",
+				Age:       30,
+				CreatedAt: int(time.Now().UnixNano()),
+			}
+
+			ids, err99 := db.Create(context.Background(), *threadID, "Persons", client.Instances{alice})
+			if err99 != nil {
+				panic(err99)
+			}
+			fmt.Println("> Success!", ids[0])
+		} else {
+			fmt.Println("alice", alice[0])
+		}
+
+		// info for computer 2
+		dbs, errDbs2 := db.ListDBs(context.Background())
+		if errDbs2 != nil {
+			panic(errDbs2)
+		}
+
+		for threadID := range dbs {
+			fmt.Println("thread", threadID)
+
+			dbInfo, errInfo := db.GetDBInfo(context.Background(), threadID)
+			if errInfo != nil {
+				panic(errInfo)
+			}
+			fmt.Println("addrs", dbInfo.Addrs)
+			fmt.Println("key", dbInfo.Key)
+
+			collections, errCol := db.ListCollections(context.Background(), threadID)
+			if errCol != nil {
+				panic(errCol)
+			}
+			for one := range collections {
+				fmt.Println("collection> ", one)
+			}
+
+		}
+	} else {
+		// computer 2
+		addr, err22 := ma.NewMultiaddr("/ip4/100.67.149.7/tcp/4006/p2p/12D3KooWEjMr3DccgP4tvUvpLEgfi7XEDDF1pxQ9dZimWLQPS67c/thread/bafk336kt326667ejcrpupk772wyfpjoik35wicwqyyn77d3fabnzdly")
+		if err22 != nil {
+			panic(err22)
+		}
+
+		key, err21 := thread.KeyFromString("bzfv6hms2bu4pqeedo333x3klnqcisszzqavq4mdquvm5sjqmwlvwtfhxe746yaoskqnxxa5hh3wwtm36otd3mkihwyteb7qe6ptgnky")
+		if err21 != nil {
+			panic(err21)
+		}
+
+		err20 := db.NewDBFromAddr(context.Background(), addr, key)
+		if err20 != nil {
+			panic(err20)
+		}
 	}
-
-	key, err21 := thread.KeyFromString("bm66r4d64fbrsrzsjpyjn7pwqznr3zvxbbm3w5mtsr6mcn5lymi3oxtz5kklur4r274xvbbimqbcjzcdegnigeyujq5tdreonqkyhv3y")
-	if err21 != nil {
-		panic(err21)
-	}
-
-	err20 := db.NewDBFromAddr(context.Background(), addr, key)
-	if err20 != nil {
-		panic(err20)
-	}
-
-
-	// reflector := jsonschema.Reflector{}
-	// mySchema := reflector.Reflect(&Person{}) // Generate a JSON Schema from a struct
-
-	// err15 := db.NewCollection(context.Background(), threadID, db2.CollectionConfig{
-	// 	Name:    "Persons",
-	// 	Schema:  mySchema,
-	// 	Indexes: []db2.Index{{
-	// 		Path:   "name", // Value matches json tags
-	// 		Unique: true, // Create a unique index on "name"
-	// 	}},
-	// })
-	// if err15 != nil {
-	// 	panic(err15)
-	// }
-
-	   /*
-err := db.UpdateCollection(context.Background(), myThreadID, db.CollectionConfig{
-    Name:    "Persons",
-    Schema:  mySchema,
-    Indexes: []db.Index{{
-        Path:   "name",
-        Unique: true,
-    },
-    {
-        Path: "created_at", // Add an additional index on "created_at"
-    }},
-	})
-	*/
-
-	/*
-	alice := &Person{
-		ID:        "",
-		Name:      "Alice",
-		Age:       30,
-		CreatedAt: int(time.Now().UnixNano()),
-	}
-
-	ids, err := db.Create(context.Background(), threadID, "Persons", client.Instances{alice})
-	fmt.Println("> Success!", ids[0])
-	*/
-
-
-	// query := db2.Where("name").Eq("Alice")
-	// results, err16 := db.Find(context.Background(), threadID, "Persons", query, &Person{})
-	// if err16 != nil {
-	// 	panic(err16)
-	// }
-
-
-	// alice := results.([]*Person)
-
-	// fmt.Println("len", len(alice))
-
-	// if len(alice) > 0 {
-	// 	fmt.Println("> Success!", alice[0])
-	// }
 }
